3ec7d4a72ce36ccdb440845630362e3cb840ecab
Bobby Nicoloulias <bnicoloulias@gmail.com>
2022-01-09 16:18:18 -0500

    End of Lecture 7: ViewModifier Animation

--- Memorize/Views/EmojiMemoryGameView.swift
+++ Memorize/Views/EmojiMemoryGameView.swift
@@ -26,18 +26,18 @@ struct EmojiMemoryGameView: View {
 		.padding(.horizontal)
 	}
 	
-//	@ViewBuilder
-//	private func cardView(for card: EmojiMemoryGame.Card) -> some View {
-//		if card.isMatched && !card.isFaceUp {
-//			Rectangle().opacity(0)
-//		} else {
-//			CardView(card: card)
-//				.padding(4)
-//				.onTapGesture {
-//					game.chooseCard(card)
-//				}
-//		}
-//	}
+	//	@ViewBuilder
+	//	private func cardView(for card: EmojiMemoryGame.Card) -> some View {
+	//		if card.isMatched && !card.isFaceUp {
+	//			Rectangle().opacity(0)
+	//		} else {
+	//			CardView(card: card)
+	//				.padding(4)
+	//				.onTapGesture {
+	//					game.chooseCard(card)
+	//				}
+	//		}
+	//	}
 	
 }
 
@@ -47,35 +47,33 @@ struct CardView: View {
 	var body: some View {
 		GeometryReader { geometry in
 			ZStack {
-				let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-				if card.isFaceUp {
-					shape.fill().foregroundColor(.white)
-					shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-//					Pie(startAngle: Angle(radians: 3 * Double.pi / 2), endAngle: Angle(radians: Double.pi))
-					Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
-						.padding(DrawingConstants.circlePadding)
-						.opacity(DrawingConstants.circleOpacity)
-					Text(card.content).font(font(in: geometry.size))
-				} else if card.isMatched {
-					shape.opacity(DrawingConstants.zeroOpacity)
-				} else {
-					shape.fill()
-				}
+				Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
+					.padding(DrawingConstants.circlePadding)
+					.opacity(DrawingConstants.circleOpacity)
+				Text(card.content)
+					.rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+					.animation(
+						Animation.linear(duration: DrawingConstants.contentAnimationDuration)
+								.repeatForever(autoreverses: false)
+					)
+					.font(Font.system(size: DrawingConstants.fontSize))
+					.scaleEffect(scale(thatFits: geometry.size))
 			}
+			.cardify(isFaceUp: card.isFaceUp)
 		}
 	}
 	
-	private func font(in size: CGSize) -> Font {
-		Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+	private func scale(thatFits size: CGSize) -> CGFloat {
+		min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
 	}
 	
 	private struct DrawingConstants {
-		static let cornerRadius: CGFloat = 10
-		static let lineWidth: CGFloat = 3
 		static let fontScale: CGFloat = 0.70
+		static let fontSize: CGFloat = 32
 		static let zeroOpacity: Double = 0
 		static let circleOpacity: Double = 0.5
 		static let circlePadding: CGFloat = 5
+		static let contentAnimationDuration: Double = 1
 	}
 }
 
